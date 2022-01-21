ad8851f0c4f779ba16f808c7a8a41043846eed4c
Kevin Marshall <info@falicon.com>
2022-01-21 07:31:07 -0500

    initial side movement

--- lib/models/config_settings.dart
+++ lib/models/config_settings.dart
@@ -1,2 +1,20 @@
 // GLOBAL CONFIGURATION VALUES
 // const api_domain = 'http://localhost:8001';
+
+words = {
+  "sun": "The Earth revolves around it.",
+  "ant": "A small insect, often found in a hill or colony.",
+  "map": "A representation of an area of land.",
+  "dog": "Man's best friend.",
+  "cat": "The internet's favorite animal.",
+  "baby": "A very young child.",
+  "boat": "A small floating vessel.",
+  "bowl": "What you eat soup out of.",
+  "gift": "A present.",
+  "game": "A form of play or sport.",
+  "false": "The opposite of true.",
+  "heavy": "Of great weight.",
+  "magic": "Mysterious tricks, performed as entertainment.",
+  "river": "A flowing body of water.",
+  "third": "Place after second."
+}
--- lib/screens/game/game.dart
+++ lib/screens/game/game.dart
@@ -17,77 +17,176 @@ class Game extends StatefulWidget {
 // ########################################
 class _GameState extends State<Game> {
   // Set up the initial tiles 
-  late List boxes = [
-    {'top': 0.0, 'left': 0.0, 'letter': 'A'},
-    {'top': 0.0, 'left': 55.0, 'letter': 'B'},
-    {'top': 0.0, 'left': 110.0, 'letter': 'C'},
+  late List _boxes = [
+    [
+      {'top': 0.0, 'left': 0.0, 'letter': 'A'},
+      {'top': 0.0, 'left': 55.0, 'letter': 'B'},
+      {'top': 0.0, 'left': 110.0, 'letter': 'C'}
+    ],
 
-    {'top': 55.0, 'left': 0.0, 'letter': 'D'},
-    {'top': 55.0, 'left': 55.0, 'letter': 'E'},
-    {'top': 55.0, 'left': 110.0, 'letter': 'F'},
+    [
+      {'top': 55.0, 'left': 0.0, 'letter': 'D'},
+      {'top': 55.0, 'left': 55.0, 'letter': 'E'},
+      {'top': 55.0, 'left': 110.0, 'letter': 'F'}
+    ],
 
-    {'top': 110.0, 'left': 0.0, 'letter': 'G'},
-    {'top': 110.0, 'left': 55.0, 'letter': 'H'},
-    {'top': 110.0, 'left': 110.0, 'letter': ''}
+    [
+      {'top': 110.0, 'left': 0.0, 'letter': 'G'},
+      {'top': 110.0, 'left': 55.0, 'letter': 'H'},
+      {'top': 110.0, 'left': 110.0, 'letter': ''}
+    ]
   ];
 
   int _animation_speed = 500;
+
+  double _box_size = 50.0;
+  double _padding = 5.0;
+  double _column_count = 3;
+  double _game_size = 400.0;
+  double _row_count = 3;
 
   @override
   void initState() {
     super.initState();
   }
 
-  void changePosition(int box_slot) {
-    if (box_slot == 0) {
-      boxes[0]['left'] = 55.0;
-      boxes[1]['left'] = 110.0;
+  void changePosition(int row, int column) {
+    // if user clicked empty square; don't move anything
+    if (_boxes[row][column]['letter'] != '') {
+      // this is a letter box; determine where the empty box is in the grid (row, column)
+      List empty_slot = [];
+      for (var r = 0; r < _boxes.length; r++) {
+        for (var c = 0; c < _boxes[r].length; c++) {
+          if (_boxes[r][c]['letter'] == '') {
+            empty_slot = [r, c];
+          }
+        }
+      }
+
+      // empty slot has to be in the same column or row of the clicked box to allow for a move
+      if (empty_slot[0] == row || empty_slot[1] == column) {
+        // we can move boxes! So determine what direction we should move
+        if (empty_slot[0] == row) {
+          // we need to move right or left (determine which)
+          num boxes_to_move = column - empty_slot[1];
+
+          // int boxes_to_move = row - empty_slot[1];
+          if (boxes_to_move < 0) {
+            // move boxes left (empty space right)
+            boxes_to_move *= -1;
+
+            // move the letters that need moved
+            int _slot = 0;
+            for (var x = 1; x <= boxes_to_move; x++) {
+              _slot = empty_slot[1] - x;
+              if (_slot < 0) {
+                _slot = _boxes[row].length - 1;
+              }
+              _boxes[row][_slot]['left'] += (_box_size + _padding);
+            }
+
+            // reset the order on this row
+            List temp = new List.from(_boxes[row]);
+
+            // move empty slot to clicked location
+            _boxes[row][column] = temp[empty_slot[1]];
+
+            // move the letter boxes
+            for (int x = 0; x < boxes_to_move; x++) {
+              num slot = column + x;
+              if (slot >= _boxes[row].length) {
+                slot -= _boxes[row].length;
+              }
+              _boxes[row][slot + 1] = temp[slot.toInt()];
+            }
+
+          } else {
+            // move the letters that need moved
+            int _slot = 0;
+            for (var x = 1; x <= boxes_to_move; x++) {
+              _slot = empty_slot[1] + x;
+              if (_slot > _boxes[row].length) {
+                _slot = 0;
+              }
+              _boxes[row][_slot]['left'] -= (_box_size + _padding);
+            }
+
+            // reset the order on this row
+            List temp = new List.from(_boxes[row]);
+
+            // move empty slot to clicked location
+            _boxes[row][column] = temp[empty_slot[1]];
+
+            // move the letter boxes
+            for (int x = 0; x < boxes_to_move; x++) {
+              num slot = column - x;
+              if (slot < 0) {
+                slot -= _boxes[row].length;
+              }
+              _boxes[row][slot - 1] = temp[slot.toInt()];
+            }
+
+          }
+
+        } else {
+          // we need to move up or down
+          print('move up or down');
+        }
+
+        // move all the boxes (and white space) that need moved
+      } else {
+        print('no move');
+      }
+
     } else {
-      // we know what box was clicked; 
-      // so determine it's location in the grid;
-      // determine where the empty box is in the grid
-      // determine direction we need to move
-      // move all the proper boxes the correct amounts
-      boxes[box_slot]['top'] = 55.0;
+      print('no move');
+
     }
+
     setState(() { });
   }
 
   List<Widget> _buildBoxes() {
     List<AnimatedPositioned> _tiles = [];
-    for (var i = 0; i < boxes.length; i++) {
-      _tiles.add(
-        AnimatedPositioned(
-          top: boxes[i]['top'],
-          left: boxes[i]['left'],
-          duration: Duration(milliseconds: _animation_speed),
-          child: InkWell(
-            onTap: () => changePosition(i),
-            child: Container(
-              alignment: Alignment.center,
-              width: 50,
-              height: 50,
-              child: Text(boxes[i]['letter'], style: TextStyle(color: Color(0xff000000))),
-              color: Color(0xffFFFFFF)
+    for (var row = 0; row < _boxes.length; row++) {
+      for (var column = 0; column < _boxes[row].length; column++) {
+        Color _background_color = Color(0xffFFFFFF);
+        if (_boxes[row][column]['letter'] == '') {
+          _background_color = Color(0xff000000);
+        }
+        if (_boxes[row][column]['letter'] != '') {
+          _tiles.add(
+            AnimatedPositioned(
+              top: _boxes[row][column]['top'],
+              left: _boxes[row][column]['left'],
+              duration: Duration(milliseconds: _animation_speed),
+              child: InkWell(
+                onTap: () => changePosition(row, column),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: _box_size,
+                  height: _box_size,
+                  child: Text(_boxes[row][column]['letter'], style: TextStyle(color: Color(0xff000000))),
+                  color: _background_color
+                )
+              )
             )
-          )
-        )
-      );
+          );
+        }
+      }
     }
     return _tiles;
   }
 
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar(title: const Text('AnimatedPositioned'));
-    final topPadding = MediaQuery.of(context).padding.top;
-
-    // AnimatedPositioned animates changes to a widget's position within a Stack
     return Scaffold(
-      appBar: appBar,
+      appBar: AppBar(
+        title: const Text('Cross Puzzle')
+      ),
       body: SizedBox(
-        height: 400,
-        width: 400,
+        height: _game_size,
+        width: _game_size,
         child: Stack(
           children: _buildBoxes()
         )
